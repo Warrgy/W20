645da75fe95cebb887f52160c35ab74182ca308e
Cameron Fisher <cfishe08@fred.socs.uoguelph.ca>
2020-01-09 23:23:03 -0500

    Finished make

--- assign0/src/LinkedListAPI.c
+++ assign0/src/LinkedListAPI.c
@@ -1,4 +1,4 @@
-#include "LinkedListAPI.h"
+#include "../include/LinkedListAPI.h"
 #include "assert.h"
 
 /** Function to initialize the list metadata head to the appropriate function pointers. Allocates memory to the struct.
