76ee85e38035e9d8cde29ff0517da702467ac1b5
Cameron Fisher <cfishe08@fred.socs.uoguelph.ca>
2020-03-06 19:47:47 -0500

    Finished Brute Force implementation for anagrams.

--- Algorithms/assign3/P11.c
+++ Algorithms/assign3/P11.c
@@ -14,33 +14,66 @@
 
 char* getInput();
 
-bool checkAnagram(char* indicator, char* toBeChecked) {
-    if (strlen(indicator) != strlen(toBeChecked)) {
+//Check if the character has already been found in the table. If it hasnt and still hasnt been found, then set that spot to a placeholder '+'.
+void checkChar(char* table, char c, unsigned int tableLength) {
+    for (int i = 0; i < tableLength; i++) {
+        if (table[i] == c) {
+            table[i] = '+';
+            break;
+        } 
+    }
+}
+
+//Check each character in the string
+void fillInTable(char* table, char* string) {
+    for (int i = 0 ; i < strlen(string); i++) {
+        checkChar(table, string[i], strlen(string));
+    }
+}
+
+//Create table for string, then using that table determine if the two strings are anagrams.
+bool checkAnagram(char* input, char* toBeChecked) {
+    if (strlen(input) != strlen(toBeChecked)) {
         return false;
     }
-    int len = strlen(indicator);
+    char* table = malloc(sizeof(bool) * strlen(input));
+    
+    for (int i = 0; i < strlen(input); i++) {
+        table[i] = input[i];
+    }
 
-    char* buffer = malloc(sizeof(char) * strlen(toBeChecked) + 1);
-    strcpy(buffer, toBeChecked);
+    fillInTable(table, toBeChecked);
 
-    for (int i = 0; i < len; i++) {
-        for(int j = 0; j < len; j++) {
+    unsigned int count = 0;
+    for (int i = 0; i < strlen(input); i++) {
+        if (table[i] == '+') {
+            count++;
         }
     }
-    free(buffer);
+    free(table);
+
+    if (count == strlen(input)) {
+        return true;
+    }
+
     return false;
 }
 
+//Find the total amount of anagrams
 char** BruteForceAnagram(char** array, unsigned int length, unsigned int* numAnagrams) {
     char* anagramString = getInput();
+    char** Anagrams = malloc(1);
 
     for(int i = 0; i < length; i++) {
         if (checkAnagram(anagramString, array[i])) {
             //Add anagram to collection
             (*numAnagrams)++;
+            Anagrams = realloc(Anagrams, sizeof(char*) * (*numAnagrams));
+            Anagrams[(*numAnagrams) - 1] = malloc(sizeof(char) * strlen(array[i]));
+            strcpy(Anagrams[(*numAnagrams) - 1], array[i]);
         }
     }
 
     free(anagramString);
-    return NULL;
+    return Anagrams;
 }
