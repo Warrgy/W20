67c71b5843169485705e6800cc6b9b765440e628
Cameron Fisher <cfishe08@fred.socs.uoguelph.ca>
2020-03-21 12:31:13 -0400

    Finished parser for file.

--- Algorithms/assign4/main.c
+++ Algorithms/assign4/main.c
@@ -11,12 +11,16 @@
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
+#include <ctype.h>
 
 #define WORD_AMOUNT 2045
 #define LINE_AMOUNT 204
 
 void free2DArray(char** words) {
     for (int i = 0; i < WORD_AMOUNT; i++) {
+        if (words[i] == NULL) {
+            break;
+        }
         free(words[i]);
     }
     free(words);
@@ -24,7 +28,10 @@ void free2DArray(char** words) {
 
 void printWords(char** words) {
     for (int i = 0; i < WORD_AMOUNT; i++) {
-        printf("[%d] %s\n", i, words[i]);
+        if (words[i] == NULL) {
+            break;
+        }
+        printf("[%d] {%s}\n", i, words[i]);
     }
 }
 
@@ -32,35 +39,47 @@ char** parseFile() {
     FILE *fp = fopen("data_7.txt", "r");
     if (fp == NULL) {
         printf("Unable to find file data_7.txt\n");
-        exit(-1);
+        return NULL;
     }
 
     char** words = malloc(sizeof(char*) * WORD_AMOUNT);
     unsigned int wordCount = 0;
     char line[256];
 
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < LINE_AMOUNT; i++) {
         //Get next word on line
         if (fgets(line, 256, fp) == NULL) {
             break;
         }
-        printf("line = %s\n", line);
 
-        words[wordCount] = malloc(1);
         char c;
-        c = fgetc(fp);
+        char prev = '\0';
         for (int j = 0; j < 256; j++) {
-            if (c != ' ' && c != '\n' && c != '\0') {
-                words[wordCount] = realloc(words[wordCount], strlen(words[wordCount]) + 2);
-                words[wordCount][strlen(words[wordCount])] = c;
-                words[wordCount][strlen(words[wordCount]) + 1] = '\0';
+            c = line[j];
+
+            if (c == '\n') {
+                break;
+            }
+
+            //If character then add onto existing word
+            if (islower(c) || isupper(c)) {
+                if (!isupper(prev) && !islower(prev)) {
+                    //start new word
+                    wordCount++;
+                    words[wordCount - 1] = malloc(sizeof(char) * 2);
+                    words[wordCount - 1][0] = c;
+                    words[wordCount - 1][1] = '\0';
+                } else {
+                    //add onto the word
+                    int len = strlen(words[wordCount - 1]);
+                    words[wordCount - 1] = realloc(words[wordCount - 1], len + 2);
+                    words[wordCount - 1][len] = c;
+                    words[wordCount - 1][len + 1] = '\0';
+                }
+                
             } 
-            c = fgetc(fp);
-
+            prev = c;
         }
-
-
-        words[i] = malloc(1);
     }
     fclose(fp);
     return words;
@@ -76,8 +95,10 @@ int main(int argc, char *argv[]) {
     clock_t start;
 
     char** words = parseFile();
-    printWords(words);
-    free2DArray(words);
+    if (words == NULL) {
+        return -1;
+    }
+    // printWords(words);
 
     //Dynamic Programming
     if (strcmp(argv[1],"1") == 0) {
@@ -93,6 +114,8 @@ int main(int argc, char *argv[]) {
         printf("Please enter one of the following next time: {1,2}\n");
         return -1;
     }
+
+    free2DArray(words);
 
     clock_t end = clock();
     double totalTime = (double)(end-start) / CLOCKS_PER_SEC;
